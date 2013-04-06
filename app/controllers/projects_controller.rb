a5f76faaa93dca078d811af3b8658b642a617765
schultyy <janschulte90@googlemail.com>
2013-04-06 16:46:22 +0200

    added create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,12 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
+	def create
+		@project = Project.new(params[:project])
+		if @project.save
+			redirect_to :controller => "main", :action => "index"
+		else
+			render :action => new
+		end
+	end
 end
