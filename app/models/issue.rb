06de1cac6156661e379df7e60667a5e5bea2ca48
schultyy <janschulte90@googlemail.com>
2013-04-23 19:41:31 +0200

    added custom validator

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,11 @@
 class Issue < ActiveRecord::Base
-  attr_accessible :assignee, :description, :title, :project_id
+  attr_accessible :assignee, :description, :title, :status, :project_id
   belongs_to :project
+  validates_with IssueStatusValidator
+end
+
+class IssueStatusValidator
+	def validate(record)
+		if record.
+	end
 end
