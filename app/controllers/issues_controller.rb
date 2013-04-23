dc303cd9f25d1db7ba2ce60252d2c0e3f9044acc
schultyy <janschulte90@googlemail.com>
2013-04-23 18:10:40 +0200

    issues view shows validation errors

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,19 +2,30 @@ class IssuesController < ApplicationController
 	def new
 		@issue = Issue.new
 		@projects = Project.all
-	end
-	def create
-		@issue = Issue.new(params[:issue])
-		if @issue.save
-			redirect_to :controller => "main", :action => "index"
-		else
-			render :action => new
+		respond_to do |format|
+			format.html
 		end
 	end
+
+	def create
+		@issue = Issue.new(params[:issue])
+		respond_to do |format|
+			if @issue.save
+				format.html{
+					redirect_to :controller => "main", :action => "index", :notice => 'Issue was saved'
+				}
+			else
+				@projects = Project.all
+				format.html  { render :action => :new }
+			end
+		end
+	end
+
 	def edit
 		@projects = Project.all
 		@issue = Issue.find(params[:id])
 	end
+
 	def update
 		@issue = Issue.find(params[:id])
 		if @issue.update_attributes(params[:issue])
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,6 @@
 class Issue < ActiveRecord::Base
   attr_accessible :assignee, :description, :title, :project_id
   belongs_to :project
+  validates :title, :presence => true
+  validates_presence_of :project
 end
